04db30886db9c3167db0f4607b281265cda6d1fe
dawidmarek95x <dawid.marek95x@gmail.com>
2022-09-21 16:41:14 +0200

    Refactoring contactForm component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,54 +1,45 @@
-import { useEffect, useState } from "react";
-import { Button, Form, Input, Label } from "./ContactForm.styled";
-import { nanoid } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
-import { addContact } from "redux/contactsReducer";
-import { saveToLocalStorage } from "services/localStorageSupport";
+import { useEffect } from 'react';
+import { Button, Form, Input, Label } from './ContactForm.styled';
+import { nanoid } from '@reduxjs/toolkit';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/contactsReducer';
+import { saveToLocalStorage } from 'services/localStorageSupport';
 
 const ContactForm = () => {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
   const contacts = useSelector(state => state.contacts);
   const dispatch = useDispatch();
 
-  const changeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const changeNumber = (e) => {
-    setNumber(e.target.value);
-  };
-
-  const addNewContact = (name, number) => {
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
+  const addNewContact = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const number = form.number.value;
 
     if (contacts.some(c => c.name === name)) {
       alert(`${name} is already in contacts`);
     } else if (contacts.some(c => c.number === number)) {
-      const contactWithNumber = contacts.filter(c => c.number === number);
+      const [contactWithNumber] = contacts.filter(c => c.number === number);
       alert(
-        `Number ${contactWithNumber[0].number} is already in phonebook. It belongs to ${contactWithNumber[0].name}.`
+        `Number ${contactWithNumber.number} is already in phonebook. It belongs to ${contactWithNumber.name}.`
       );
     } else {
-      dispatch(addContact(newContact));
+      dispatch(
+        addContact({
+          id: nanoid(),
+          name,
+          number,
+        })
+      );
+      form.reset();
     }
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addNewContact(name, number);
-  }
 
   useEffect(() => {
     saveToLocalStorage('contacts', contacts);
   }, [contacts]);
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={addNewContact}>
       <Label>
         Name
         <Input
@@ -56,9 +47,7 @@ const ContactForm = () => {
           name="name"
           pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          value={name}
           required
-          onChange={changeName}
         />
       </Label>
       <Label>
@@ -68,15 +57,13 @@ const ContactForm = () => {
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          value={number}
           required
-          onChange={changeNumber}
         />
       </Label>
 
       <Button type="submit">Add contact</Button>
     </Form>
-  )
-}
+  );
+};
 
 export default ContactForm;
